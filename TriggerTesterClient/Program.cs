f35236875591cf683cf4abdf7502b5cb2e42e529
Per Gustafsson <per.gustafsson1@learnet.se>
2019-11-19 16:36:24 +0100

    Initial setup of tester for trigger sending and trigger listening.

--- TriggerTesterClient/Program.cs
+++ TriggerTesterClient/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 using ElseTriggerHandler;
 
 namespace TriggerTesterClient
@@ -12,9 +13,21 @@ namespace TriggerTesterClient
         static void Main(string[] args)
         {
             Console.WriteLine("TriggerSender has started");
-            
-          
-            
+            Console.WriteLine("Press a key to ping the listener!");
+            Console.ReadKey();
+
+            for (int i = 0; i < 30; i++)
+            {
+                var client = TriggerSenderFactory.CreateTransportType(TransportType.WcfBasic);
+                string name = client.GetName("TransportManager", 2);
+
+                Thread.Sleep(1000);
+                client.Send(name);
+
+                Console.WriteLine();
+                Console.WriteLine("Pinged {0}", i);
+            }
+
         }
     }
 }
--- TriggerTesterListener/Program.cs
+++ TriggerTesterListener/Program.cs
@@ -11,9 +11,23 @@ namespace TriggerTesterListener
     {
         static void Main(string[] args)
         {
-            TriggerListenerFactory.CreateTransportType(TransportType.Ewh);
+            TimeSpan interval = new TimeSpan(0, 0, 3);
+            var test = TriggerListenerFactory.CreateTransportType(TransportType.WcfBasic);
+            test.SetUp("TransportManager", 2);
 
             Console.WriteLine("TriggerListener has been initiated!");
+                        
+            //Console.ReadKey();
+
+
+            while (true)
+            {
+                if (!test.Wait(interval))
+                {
+                    Console.WriteLine("No ping for 3 seconds");
+                }
+               
+            }
         }
     }
 }
